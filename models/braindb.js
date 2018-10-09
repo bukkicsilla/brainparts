549f49ba38c8252cbc5f989e211ac09f5c88b27a
csilla <csilla.bukki@gmail.com>
2018-10-08 17:58:25 -0700

    heroku mlab 2

--- models/braindb.js
+++ models/braindb.js
@@ -10,7 +10,8 @@ if (process.env.NODE_ENV === 'production'){
     //dbURI = 'mongodb://heroku_l1zxvgh6:2tvfgoopgt9fhq8imhgfkjo7cj@ds123399.mlab.com:23399/heroku_l1zxvgh6';
 }
 //NODE_ENV=production MONGODB_URI=mongodb://heroku_l1zxvgh6:2tvfgoopgt9fhq8imhgfkjo7cj@ds123399.mlab.com:23399/heroku_l1zxvgh6 nodemon start
-mongoose.connect(dbURI);
+//mongoose.connect(dbURI);
+mongoose.connect('mongodb://lunasaturni:vorce166@ds143451.mlab.com:43451/brainmlab');
 
 mongoose.connection.on('connected', function(){
     console.log('Moongose connected to ' + dbURI);
