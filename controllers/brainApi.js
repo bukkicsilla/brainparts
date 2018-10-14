d05bab0a580dec33ec83a865d65f61ffad04fecc
csilla <csilla.bukki@gmail.com>
2018-10-13 20:23:26 -0700

    delete part in API

--- controllers/brainApi.js
+++ controllers/brainApi.js
@@ -48,3 +48,23 @@ module.exports.createPart = function(req, res){
         }
     });
 }
+
+module.exports.deletePart = function(req, res){
+    var partid = req.params.brainpartid;
+    if (partid){
+        Brain.findByIdAndRemove(partid)
+           .exec(
+                function(err, part){
+                    if(err){
+                        res.status(404);
+                        res.json(err);
+                        return;
+                    }
+                    res.status(204);
+                    res.json(null);
+                });
+    } else {
+        res.status(404);
+        res.json({"message":"no id"});
+    } 
+} 
--- routes/indexApi.js
+++ routes/indexApi.js
@@ -5,5 +5,6 @@ var brainparts = require('../controllers/brainApi');
 router.get('/brainparts', brainparts.allParts);
 router.get('/brainparts/:brainpartid', brainparts.showPart);
 router.post('/brainparts', brainparts.createPart);
+router.delete('/brainparts/:brainpartid', brainparts.deletePart);
 
 module.exports = router;
